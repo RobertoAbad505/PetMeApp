71ce28ac451473176fb1b4253ef917d773c6f113
Roberto Abad <roberto.rmzabad>
2025-08-02 11:43:59 -0600

    Initial Commit

---
+++ PetMeApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PetMeApp
+//
+//  Created by Roberto Ramirez on 8/2/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
